2702f8623692e9c41aa5d8d707f6d43461cb1270
Marcos <marcos.ponts@hotmail.com>
2022-02-09 17:19:52 -0300

    Project creation and README.md

---
+++ src/main/java/com/beer_Api_Test/beerStock/BeerStockApplication.java
@@ -0,0 +1,13 @@
+package com.beer_Api_Test.beerStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beer_Api_Test/beerStock/BeerStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beer_Api_Test.beerStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
