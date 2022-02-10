e08f796b0fea5d45662029645863b640ddeb739c
Marcos <marcos.ponts@hotmail.com>
2022-02-10 18:40:23 -0300

    finalized package BeerService.

--- src/main/java/com/beer_Api_Test/beerStock/repository/BeerRepository.java
+++ src/main/java/com/beer_Api_Test/beerStock/repository/BeerRepository.java
@@ -7,5 +7,7 @@ import com.beer_Api_Test.beerStock.entity.Beer;
 
 public interface BeerRepository extends JpaRepository<Beer, Long> {  //class responsavel em conectar com o banco de dados + o JpaRepository, resumo:CRUD.
 
-	Optional<Beer> findOptional(String name); //Optional é um meio de validações.
+	Optional<Beer> findByName(String name); //Optional é um meio de validações.
+
+
 }
--- src/main/java/com/beer_Api_Test/beerStock/service/BeerService.java
+++ src/main/java/com/beer_Api_Test/beerStock/service/BeerService.java
@@ -1,10 +1,17 @@
 package com.beer_Api_Test.beerStock.service;
 
+import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.beer_Api_Test.beerStock.dto.BeerDTO;
+import com.beer_Api_Test.beerStock.entity.Beer;
 import com.beer_Api_Test.beerStock.exception.BeerAlreadyRegisteredException;
+import com.beer_Api_Test.beerStock.exception.BeerNotFoundException;
+import com.beer_Api_Test.beerStock.exception.BeerStockExceededException;
 import com.beer_Api_Test.beerStock.mapper.BeerMapper;
 import com.beer_Api_Test.beerStock.repository.BeerRepository;
 
@@ -18,7 +25,52 @@ public class BeerService {
 	private final BeerMapper beerMapper = BeerMapper.INSTANCE;
 	
 	public BeerDTO createBeer(BeerDTO beerDTO) throws BeerAlreadyRegisteredException {
-		return beerDTO;
-		
+		verifyIfIsAlreadyRegistered(beerDTO.getName());
+		Beer beer = beerMapper.toModel(beerDTO);
+		Beer savedBeer = beerRepository.save(beer);
+		return beerMapper.toDTO(savedBeer);
 	}
+	
+	public BeerDTO findByName(String name) throws BeerNotFoundException{
+		Beer foundBeer = beerRepository.findByName(name)
+				.orElseThrow(() -> new BeerNotFoundException(name));
+		return beerMapper.toDTO(foundBeer);
+	}
+	
+	public List<BeerDTO> listAll(){
+		return beerRepository.findAll()
+				.stream()
+				.map(beerMapper::toDTO)
+				.collect(Collectors.toList());
+	}
+	
+	public void deletebyId(Long id) throws BeerNotFoundException{
+		verifyIfExists(id);
+		beerRepository.deleteById(id);
+	}
+
+	private void verifyIfIsAlreadyRegistered(String name) throws BeerAlreadyRegisteredException {
+		Optional<Beer> optSavedBeer = beerRepository.findByName(name);
+		if (optSavedBeer.isPresent()) {
+			throw new BeerAlreadyRegisteredException(name);
+		}
+	}
+	
+	private Beer verifyIfExists(Long id) throws BeerNotFoundException {
+		return beerRepository.findById(id)
+				.orElseThrow(() -> new BeerNotFoundException(id));
+	}
+	
+	public BeerDTO increment(Long id, int quantityToIncrement) throws BeerNotFoundException, BeerStockExceededException{
+		Beer beerToIncrementStock = verifyIfExists(id);
+		int quantityAfterIncrement = quantityToIncrement + beerToIncrementStock.getQuantity();
+		if (quantityAfterIncrement <= beerToIncrementStock.getMax()) {
+			beerToIncrementStock.setQuantity(beerToIncrementStock.getQuantity() + quantityToIncrement);
+			Beer incrementedBeerStock = beerRepository.save(beerToIncrementStock);
+			return beerMapper.toDTO(incrementedBeerStock);
+		}
+		throw new BeerStockExceededException(id, quantityToIncrement);
+	}
+	
+	
 }
